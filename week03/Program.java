64097a3e0e573b1bd1bb2f95e2d5c01038a2d8f5
Dawid Sobczak <dawidsobczak@fastmail.com>
2022-02-08 16:33:44 +0000

    week03

---
+++ week03/Program.java
@@ -0,0 +1,18 @@
+//int[] arr = {10,2,5,3}
+//int[] arr = {7,1,14,11}
+//int[] arr = {3,1,7,11}
+
+class Program {
+    public static void main(String[] args) {
+        int[] arr = {7,1,14,11};
+        for(int i = 0; i < arr.length; i++) {
+            int n = arr[i];
+            for(int j = 0; j < arr.length; j++) {
+                int m = arr[j];
+                if(m * 2 == n) {
+                    System.out.println("The number " + n + " is 2 times " + m);
+                }
+            }
+        }
+    }
+}
